37a21965345dd2734218184e8d30976cede7e0f2
gowthamkumar.j <gowthamkumar.j@zohocorp.com>
2024-09-01 20:14:45 +0530

    wip: gh config

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/gowtham-20595/Local/Archive_of_gowtham/gowthamk17/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/gowtham-20595/Local/Archive_of_gowtham/gowthamk17/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/gowtham-20595/Local/Archive_of_gowtham/gowthamk17/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/gowtham-20595/Local/Archive_of_gowtham/gowthamk17/src/css/custom.css"),
+];
